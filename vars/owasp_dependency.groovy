a1a93af1103fcf65a145face0bd191192ee1d656
lalitthipe07 <thipelalit4@gmail.com>
2026-07-01 12:32:08 +0530

    Updated Jenkins pipeline and project configuration

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,21 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+
+    withCredentials([
+        string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')
+    ]) {
+
+        dependencyCheck(
+            odcInstallation: 'OWASP',
+            additionalArguments: """
+                --scan ./ \
+                --format XML \
+                --format HTML \
+                --nvdApiKey ${NVD_API_KEY}
+            """
+        )
+
+        dependencyCheckPublisher(
+            pattern: '**/dependency-check-report.xml'
+        )
+    }
 }
