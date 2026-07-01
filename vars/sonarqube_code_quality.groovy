6bec5b059400476d5eece946617da7d1bbd6b630
lalitthipe07 <thipelalit4@gmail.com>
2026-07-01 18:33:34 +0530

    sonarupdate

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,16 @@
-def call(){
-  timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+def call() {
+
+    timeout(time: 5, unit: 'MINUTES') {
+
+        def qg = waitForQualityGate(abortPipeline: false)
+
+        echo "SonarQube Quality Gate Status: ${qg.status}"
+
+        if (qg.status != 'OK') {
+            echo "WARNING: Quality Gate Failed"
+            echo "Pipeline will continue..."
+        } else {
+            echo "Quality Gate Passed"
+        }
+    }
 }
