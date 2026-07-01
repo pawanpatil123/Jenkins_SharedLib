1de63db8a71e2001f677b8fb98f9899e1720d7ed
lalitthipe07 <thipelalit4@gmail.com>
2026-07-01 17:43:43 +0530

    update owasp dependecy config

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,16 +1,17 @@
 def call() {
 
-    withCredentials([
-        string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')
-    ]) {
+    withCredentials([string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')]) {
 
         dependencyCheck(
             odcInstallation: 'OWASP',
             additionalArguments: """
-                --scan ./ \
-                --format XML \
-                --format HTML \
-                --nvdApiKey ${NVD_API_KEY}
+                --scan .
+                --format XML
+                --format HTML
+                --noupdate
+                --disableRetired
+                --disableKnownExploited
+                --nvdApiKey=${NVD_API_KEY}
             """
         )
 
@@ -19,3 +20,43 @@ def call() {
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// def call() {
+
+//     withCredentials([
+//         string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')
+//     ]) {
+
+//         dependencyCheck(
+//             odcInstallation: 'OWASP',
+//             additionalArguments: """
+//                 --scan ./ \
+//                 --format XML \
+//                 --format HTML \
+//                 --nvdApiKey ${NVD_API_KEY} \
+//                 --noupdate
+//             """
+//         )
+
+//         dependencyCheckPublisher(
+//             pattern: '**/dependency-check-report.xml'
+//         )
+//     }
+// }
